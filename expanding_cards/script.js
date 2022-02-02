cbb1b3a9e1e90895deec47ea3bc8af6dfba70a9e
전수경 <78544372+susoro@users.noreply.github.com>
2022-02-02 18:00:38 +0900

    주석

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -6,8 +6,10 @@ panels.forEach( (panel) =>{
   panel.addEventListener('click', () =>{
     removeActiveClasses()
     panel.classList.add('active')
+    // 클릭하면 뒤에껀 지워지게끔 하고 다음 이미지가 보여진다
   })
 })
+
 function removeActiveClasses(){
   panels.forEach(panel =>{
     panel.classList.remove('active')
