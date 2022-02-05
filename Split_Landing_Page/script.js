abf07ac9f126f71c04bbb83bd908deefa5c4d614
전수경 <78544372+susoro@users.noreply.github.com>
2022-02-06 00:54:02 +0900

    끝까지 했다

--- Split_Landing_Page/script.js
+++ Split_Landing_Page/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const containter = document.querySelector('.containter')
+
+left.addEventListener('mouseenter', ()=> containter.classList.add('hover-left'))
+left.addEventListener('mouseleave', ()=> containter.classList.remove('hover-left'))
+
+
+right.addEventListener('mouseenter', ()=> containter.classList.add('hover-right'))
+right.addEventListener('mouseleave', ()=> containter.classList.remove('hover-right'))
